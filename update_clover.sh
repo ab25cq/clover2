f3e6940559af0bef73c54b78328c4cdab701a001
Daisuke Minato <ab25cq@localhost.localdomain>
2016-09-10 21:36:27 +0900

    rm some files

--- update_clover.sh
+++ update_clover.sh
@@ -1,5 +1,8 @@
 make distclean
-git add .
-git commit 
-git remote add origin https://github.com/ab25cq/clover2.git
-git push -u origin master
+if [ ! -e Makefile ]
+then
+    git add .
+    git commit 
+    git remote add origin https://github.com/ab25cq/clover2.git
+    git push -u origin master
+fi
