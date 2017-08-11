578fdd8335bbdc6fdf12bda0db8fc19c26d50591
ab25q <ab25cq@gmail.com>
2017-08-11 22:46:32 +0900

    bit compiler

--- update_clover.sh
+++ update_clover.sh
@@ -6,8 +6,8 @@ if [ ! -e Makefile ]
 then
     git add .
     git commit 
-    git remote add bit-compiler https://github.com/ab25cq/clover2.git
-    git push -u origin bit-compiler
+#    git remote add bit-compiler https://github.com/ab25cq/clover2.git
+    git push origin bit-compiler
 #    git add .
 #    git commit 
 #    git remote add origin https://github.com/ab25cq/clover2.git
