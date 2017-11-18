3f64b5aa5b44cb35094347848410164b1d53b5f0
ab25q <ab25cq@gmail.com>
2017-11-18 13:45:14 +0900

    NullPointerExceptionのチェック

--- src/class_clover.c
+++ src/class_clover.c
@@ -4,6 +4,11 @@ BOOL Clover_load(CLVALUE** stack_ptr, CLVALUE* lvar, sVMInfo* info)
 {
     CLVALUE* file_name = lvar;
 
+    if(file_name->mObjectValue) {
+        entry_exception_object_with_class_name(stack_ptr, info->current_stack, info->current_var_num, info, "Exception", "Null pointer exception");
+        return FALSE;
+    }
+
     /// Clover to c value ///
     char* file_name_value = ALLOC string_object_to_char_array(file_name->mObjectValue);
 
