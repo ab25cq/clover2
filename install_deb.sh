ec483921074ba086a63b4537bb90d3509aeb7e52
ab25cq <ab25cq@clover-lang.org>
2018-02-02 14:56:08 +0900

    Debian

--- install_deb.sh
+++ install_deb.sh
@@ -8,3 +8,13 @@ then
 
     sudo dpkg -i ../clover2_3.5.6-1_i386.deb
 fi
+
+if test -e ../clover2_3.5.6-1_amd64.deb
+then
+    if dpkg -l | egrep ^clover2
+    then
+        sudo apt-get remove clover2
+    fi
+
+    sudo dpkg -i ../clover2_3.5.6-1_amd64.deb
+fi
